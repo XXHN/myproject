3876babd2b2aefa3be83c2a64391151b8f34fbe6
XXHN <598285530@qq.com>
2020-04-26 18:17:24 +0800

    first commit

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Student implements Serializable{
+	final static long serialVersionUID=1L; //L表示是一个长整形
+	String name;
+	int age;
+	
+	public Student(){
+		
+	}
+	
+	public Student(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+	
+	public void printInfo(){
+		System.out.println(name+","+age);
+	}
+}
